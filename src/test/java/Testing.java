f46fcdfa25fa47f2238a00eadee014b4c3fc2507
ryeon9445 <ryeon9445@gmail.com>
2018-07-11 22:14:52 +0900

    First Commit

---
+++ src/test/java/Testing.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by CBR on 2018. 7. 11..
+ */
+public class Testing {
+
+    @Test
+    public void testing() {
+        String hello = "Hello World";
+        Assert.assertEquals("Hello World", hello);
+    }
+
+}
